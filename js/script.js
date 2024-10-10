3d22af9cf8f3d80558977737d2c5952a59637ca9
Oliver-Rose-University <oliver.rose@student.norwichuni.ac.uk>
2024-10-10 16:15:39 +0100

    the truth of life is we live to suffer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 const myButton = document.getElementById("button1");
 const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementById("char-image");
 
 let clickCounter = 0;
 
 function clickFunction() {
     console.log("THE BUTTON HATH BEEN PRESSED!!")
     clickCounter = clickCounter +1;
-    myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
+
+    if (clickCounter > 3){
+        clickCounter = 1;
+    }
+
+    myParagraph.innerHTML = "I am on character: " + clickCounter;
+
+    if (clickCounter === 1){
+        characterImage.src = "assets/Charmander.png";
+        return;
+    }
+    if (clickCounter === 2){
+        characterImage.src = "assets/Squirtle.jpeg";
+        return;
+    }
+    if (clickCounter === 3){
+        characterImage.src = "assets/Bulbasaur.png";
+        return;
+    }
 }
 
 myButton.onclick = clickFunction;
