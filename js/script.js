33078cb93856d34d75532fcfe90867db8c11637a
Oliver-Rose-University <oliver.rose@student.norwichuni.ac.uk>
2024-10-15 12:04:06 +0100

    Made my comments more efficient

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// declaring stuff
 const myButton = document.getElementById("button1");
 const myParagraph = document.getElementById("my-paragraph");
 const characterImage = document.getElementById("char-image");
@@ -7,54 +6,21 @@ const altText = ["Initial Pokemon: Bulbasaur", "Initial Pokemon: Charmander", "I
 const paragraphText = ["I am Bulbasaur!", "I am Charmander!", "I am Squirtle!"];
 const customName = document.getElementById("input1");
 const customNameButton = document.getElementById("input-button");
-
 let clickCounter = 0;
-
-
 function onSubmitButton(){
     myParagraph.innerHTML = customName.value;
-
 }
-
-
-/**
- * 
- * @returns How intriguing, a comment in a place such as this? Hm. Indeed it shall be. God I hate doing comments. No real reason even I just really hate it.
- */
 function clickFunction() {
     console.log("THE BUTTON HATH BEEN PRESSED!!")
     clickCounter = clickCounter + 1;
-    // it increments. Nothing more to say.
     if (clickCounter > 2) {
         clickCounter = 0;
     }
     characterImage.src = characterArray[clickCounter];
     myParagraph.innerHTML = paragraphText[clickCounter];
-    // myParagraph.innerHTML = customName.value;
     characterImage.alt = altText[clickCounter];
     return;
-
-
-    // if (clickCounter === 1) {
-    //     characterImage.src = characterArray[clickCounter];
-    //     myParagraph.innerHTML = paragraphText[clickCounter];
-    //     characterImage.alt = altText[clickCounter];
-
-    //     return;
-    // }
-    // if (clickCounter === 2) {
-    //     characterImage.src = characterArray[1];
-    //     myParagraph.innerHTML = paragraphText[1];
-    //     characterImage.alt = altText[1];
-    //     return;
-    // }
-    // if (clickCounter === 3) {
-    //     characterImage.src = characterArray[2];
-    //     myParagraph.innerHTML = paragraphText[2];
-    //     characterImage.alt = altText[2];
-    //     return;
-    // }
     }
-
 myButton.onclick = clickFunction;
 customNameButton.onclick = onSubmitButton;
+// CODE DOCUMENTATION: https://nua-my.sharepoint.com/:w:/r/personal/oliver_rose_student_norwichuni_ac_uk/Documents/Code%20Documentation%201.docx?d=w09777bde6989425ea6c66b3bf5ac7708&csf=1&web=1&e=ftoxIr
